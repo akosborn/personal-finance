06f46094f064ac6d6a141f6628be32fcddde41a4
akosborn <andrewosborn93@gmail.com>
2018-06-05 16:56:58 -0400

    added wallet to dashboard.component

--- src/app/shared/wallet.service.ts
+++ src/app/shared/wallet.service.ts
@@ -1,13 +1,17 @@
-import {Injectable, OnInit} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Wallet} from './wallet.model';
 import {Subject} from 'rxjs';
 
 @Injectable()
 export class WalletService {
   private wallet: Wallet = new Wallet(1250.6, 265.73, 24000.30);
-  private walletChanged = new Subject<Wallet>();
+  private walletSubject: Subject<Wallet> = new Subject<Wallet>();
 
   constructor() { }
+
+  getWalletSubject(): Subject<Wallet> {
+    return this.walletSubject;
+  }
 
   getWallet(): Wallet {
     return this.wallet;
@@ -15,6 +19,6 @@ export class WalletService {
 
   updateWallet(wallet: Wallet) {
     this.wallet = wallet;
-    this.walletChanged.next(wallet);
+    this.walletSubject.next(wallet);
   }
 }
