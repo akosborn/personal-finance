dffe172a14effa525b3458fa5626655fad06accb
akosborn <andrewosborn93@gmail.com>
2018-06-06 10:23:10 -0400

    setup home and dashboard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
